c4bfbaab14ef8df5cd4ba18d0bf6728114e733b6
Karen Garcia <kgarcia@i3screen.com>
2020-05-08 16:15:32 -0600

    Sass is in there added Footer and scss files for styling

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,41 +1,27 @@
 import React, {useState} from 'react';
-import logo from '../Assests/logo.svg';
 import '../Styling/App.scss';
+import Footer from './Footer';
 
 function App() {
-    const [counter, setCounter] = useState(0);
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Editing <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <Counter counter={counter}/>
-            </header>
-        </div>
+        <>
+            {/*<DataCalculate />*/}
+            <Footer />
+        </>
     );
 }
 
-interface CounterProps {
-    counter: number;
-}
-
-export const Counter = ({counter}:CounterProps) =>{
-return(
-    <div>
-        <p>{counter}</p>
-    </div>
-)
-};
+// interface CounterProps {
+//     counter: number;
+// }
+//
+// export const Counter = ({counter}:CounterProps) =>{
+// return(
+//     <div>
+//         <p>{counter}</p>
+//     </div>
+// )
+// };
 
 export default App;
