ad969f9883d9d799b2b38debb424f2b4d6c0ef24
Karen Garcia <kgarcia@i3screen.com>
2020-05-10 20:16:35 -0600

    Updates

---
+++ precache-manifest.b05831bd9e4f6c68f392b92382d43c32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496f96117d8301717750701bcb999070",
+    "url": "/MortgageBeGone/index.html"
+  },
+  {
+    "revision": "6513f54395c587c4395a",
+    "url": "/MortgageBeGone/static/css/main.787e3e8c.chunk.css"
+  },
+  {
+    "revision": "7ba74a427934299fcce0",
+    "url": "/MortgageBeGone/static/js/2.924f1239.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MortgageBeGone/static/js/2.924f1239.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6513f54395c587c4395a",
+    "url": "/MortgageBeGone/static/js/main.859c7d01.chunk.js"
+  },
+  {
+    "revision": "f14eb1466cb6e5bdf0b7",
+    "url": "/MortgageBeGone/static/js/runtime-main.d219db84.js"
+  },
+  {
+    "revision": "d0827d01ac17b128172a9f867b8d0c82",
+    "url": "/MortgageBeGone/static/media/sayKaren_logo-white_8.1.2019.d0827d01.png"
+  }
+]);
